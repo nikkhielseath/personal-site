8cb1924896c69f8cee92237bcefe620280c546ee
SNikhill <snikhill@protonmail.com>
2021-05-16 21:56:45 +0530

    Setup Home Page
    Basic Setup of
    Landing Page

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: "./src/assets/",
       },
       __key: "images",
     },
