567761ae1ceeea019d4ddd5795bdeea5ed570bf8
SNikhill <snikhill@protonmail.com>
2021-05-24 02:48:01 +0530

    Create Responsive Projects-Page

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -1,41 +1,78 @@
 import React from "react";
-import CallToAction from "../../components/CallToAction/CallToAction";
+import ProjectItem from "../../components/ProjectItem/ProjectItem";
 
 import PlaceholderImg from "../../assets/placeholder.png";
-
 import "./Projects.scss";
 
-const ProjectItem = ({ name, imgURL, description }) => {
-  return (
-    <div className={"project__card"}>
-      <img className={"card__img"} alt={name} src={imgURL} />
-      <div className={"card__details"}>
-        <h4 className={"detail__title"}>{name}</h4>
-        <CallToAction className={"detail__link"}>Link</CallToAction>
-      </div>
-      <div className={"card__description"}>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-};
-
 const ProjectsPage = () => {
-  const projectName = "Name";
-  const dummyText =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-  const dummyText2 =
-    "Lorem ipsum  elit, sed eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  const Projects = [
+    {
+      name: "Dummy",
+      imgURL: PlaceholderImg,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie.",
+    },
+    {
+      name: "Dummy",
+      imgURL: PlaceholderImg,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie.",
+    },
+    {
+      name: "Dummy",
+      imgURL: PlaceholderImg,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie.",
+    },
+    {
+      name: "Dummy",
+      imgURL: PlaceholderImg,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie.",
+    },
+    {
+      name: "Dummy",
+      imgURL: PlaceholderImg,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie." +
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie." +
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie.",
+    },
+    {
+      name: "Dummy",
+      imgURL: PlaceholderImg,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie.",
+    },
+    {
+      name: "Dummy",
+      imgURL: PlaceholderImg,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie." +
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie." +
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie.",
+    },
+  ];
   return (
     <React.Fragment>
-      <main className={"project__container"}>
-        <ProjectItem
-          name={projectName}
-          description={dummyText}
-          imgURL={PlaceholderImg}
-        />
-      </main>
+      <div className={"page"}>
+        <header>
+          <a>Home</a>
+        </header>
+        <main className={"project"}>
+          <div className={"project__hero"}>
+            <h2 className={"hero__title"}>Projects</h2>
+            <h6 className={"hero__phrase"}>
+              Crafts that I was lucky to be a part of.
+            </h6>
+          </div>
+          <div className={"project__container"}>
+            {Projects.map((props) => (
+              <ProjectItem {...props} />
+            ))}
+          </div>
+        </main>
+      </div>
     </React.Fragment>
   );
 };
