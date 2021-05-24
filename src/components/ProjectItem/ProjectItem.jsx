57ef02577431fd51a0e40c3f758c67fc126adac3
SNikhill <snikhill@protonmail.com>
2021-05-24 22:30:48 +0530

    Integrate Dato CMS
    Setup Dato CMS
    for the Projects
    Page

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import CallToAction from "../CallToAction/CallToAction";
 
+import PlaceholderImage from "../../assets/placeholder.png";
 import "./ProjectItem.scss";
 
-const ProjectItem = ({ name, imgURL, description }) => {
+const ProjectItem = ({ name = "Name", link, image, description }) => {
+  let imgAlt = name,
+    imgSrc = PlaceholderImage;
+  if (image) {
+    imgAlt = image.alt;
+    imgSrc = `${image.url}&w=312`;
+  }
   return (
     <div className={"project__card"}>
-      <img className={"card__img"} alt={name} src={imgURL} />
+      <img className={"card__img"} alt={imgAlt} src={imgSrc} />
       <div className={"card__details"}>
-        <h4 className={"detail__title"}>{name}</h4>
-        <CallToAction className={"detail__link"} type={"external"}>
+        <h5 className={"detail__title"}>{name}</h5>
+        <CallToAction
+          className={"detail__link"}
+          type={"external"}
+          href={link}
+          target={"_blank"}
+          rel={"nofollow noreferrer"}
+        >
           Link
         </CallToAction>
       </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ProjectsPage from "../views/Projects/Projects";
+import { graphql } from "gatsby";
 
-const Projects = () => {
+const Projects = ({ data }) => {
   return (
     <React.Fragment>
-      <ProjectsPage />
+      <ProjectsPage projects={data.allDatoCmsProject.edges} />
     </React.Fragment>
   );
 };
 
 export default Projects;
+export const query = graphql`
+  query ProjectsQuery {
+    allDatoCmsProject {
+      edges {
+        node {
+          image {
+            alt
+            url
+          }
+          name
+          link
+          description
+        }
+      }
+    }
+  }
+`;
