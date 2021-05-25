45191ee49f90ce9671e312c92b621a328470d7fc
SNikhill <snikhill@protonmail.com>
2021-05-25 15:32:56 +0530

    Create SEO Component

---
+++ src/components/SEO/socialLinks.js
@@ -0,0 +1,12 @@
+const SOCIAL_LINKS = [
+  { handle: "twitter", url: "https://twitter.com/SethNikhill" },
+  {
+    handle: "github",
+    url: "https://github.com/SNikhill",
+  },
+  {
+    handle: "youtube",
+    url: "https://www.youtube.com/devicenerd",
+  },
+];
+export default SOCIAL_LINKS;
