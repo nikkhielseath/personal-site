44fa087e858e580ac2cf00853b061c804432394b
SNikhill <snikhill@protonmail.com>
2021-07-01 14:45:12 +0530

    Implement Parallax Scroll #20

--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -13,14 +13,14 @@ const ProjectsPage = ({ projects }) => {
             <Link to={"/"}>Home</Link>
           </nav>
         </header>
-        <main className={"project"}>
-          <div className={"project__hero"}>
+        <main className={"project parallax"}>
+          <div className={"project__hero parallax__background"}>
             <h1 className={"hero__title"}>Projects</h1>
             <h6 className={"hero__phrase"}>
               Crafts that I was lucky to be a part of.
             </h6>
           </div>
-          <div className={"project__container"}>
+          <div className={"project__container parallax__foreground"}>
             {projects.map(({ node }, index) => {
               return <ProjectItem {...node} key={`project_${index}`} />;
             })}
