3e6ec70149b473a02b34fe2c29969e69d3b5de1c
SNikhill <snikhill@protonmail.com>
2022-04-24 20:07:16 +0530

    chore(blog): change SEO details
    Change SEO details
    for each blog post

--- src/views/BlogPost/BlogPost.jsx
+++ src/views/BlogPost/BlogPost.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import Page from "../../components/Layout/Page/Page";
+import SEO from "../../components/SEO/SEO";
 
 import "./BlogPost.scss";
 
 const BlogPost = ({ markdownRemark }) => {
   const { frontmatter, html } = markdownRemark;
+  const { title, subtitle } = frontmatter;
   return (
-    <Page
-      heroTitle={frontmatter.title}
-      heroPhrase={frontmatter.subtitle}
-      mainClassName="blogPost"
-      heroClassName=""
-    >
-      <div
-        className="blogPost__content"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-    </Page>
+    <>
+      <SEO title={title} description={subtitle} />
+      <Page
+        heroTitle={title}
+        heroPhrase={subtitle}
+        mainClassName="blogPost"
+        heroClassName=""
+      >
+        <div
+          className="blogPost__content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </Page>
+    </>
   );
 };
 
