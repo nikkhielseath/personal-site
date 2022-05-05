46979a8e69364cd0e82aa72becc06b65dd09b3d1
SNikhill <snikhill@protonmail.com>
2022-05-06 00:58:02 +0530

    fix(blog): add blog seo details
    Add meta tags
    information
    for Blog
    and Blog Post
    Page

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { graphql } from "gatsby";
+import SEO from "../components/SEO/SEO";
+
 import BlogPost from "../views/BlogPost/BlogPost";
 
 const Template = ({
   data, // this prop will be injected by the GraphQL query below.
 }) => {
   const { markdownRemark } = data; // data.markdownRemark holds your post data
-  return <BlogPost markdownRemark={markdownRemark} />;
+  return (
+    <>
+      <SEO
+        title={markdownRemark.frontmatter.title}
+        description={markdownRemark.frontmatter.subtitle}
+      />
+      <BlogPost markdownRemark={markdownRemark} />
+    </>
+  );
 };
 
 export default Template;
