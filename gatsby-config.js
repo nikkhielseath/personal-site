9f6f60ed750fe9669a80cf033a8ebb9995b266b3
SNikhill <snikhill@protonmail.com>
2022-06-07 01:19:57 +0530

    fix(#58): open external links in new tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,12 @@ module.exports = {
             },
           },
           "gatsby-remark-images-anywhere",
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+            },
+          },
         ],
       },
     },
